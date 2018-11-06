6d9b9f2614deb4ca63b0bf0fd3f084a677348ff6
RockYang <ro66102603@qq.com>
2018-11-06 12:04:39 +0800

    去除邮件通知

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -53,8 +53,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        //Auth::login($user);
-        $this->sendEmailConfirmationTo($user);
+        Auth::login($user);
+        //$this->sendEmailConfirmationTo($user);
         session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
         return redirect('/');
     }
@@ -66,9 +66,9 @@ class UsersController extends Controller
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($to, $subject) {
+        /*Mail::send($view, $data, function ($message) use ($to, $subject) {
             $message->to($to)->subject($subject);
-        });
+        });*/
     }
 
     public function edit(User $user)
